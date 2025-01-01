40856fb2faf20a227298977d64ccdcd7ab6a59d5
Ex0TiiC24 <taiyophamadadar6@gmail.com>
2025-01-01 22:30:51 +0700

    add schema

--- server.js
+++ server.js
@@ -1,17 +1,63 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const mongoosse = require('mongoose')
+// let database = []
+// let counter = 100
 
-try{
-    app.listen(port,()=>{
-        console.log("Succesfully connected to ",port)
-    })
-}
-catch{
-    console.log("failed to connect")
-}
+app.use(bodyParser.json())
+
 
 
 app.get('/',(req,res)=>{
     res.send("hi")
 })
+
+app.post('/product',(req,res)=>{
+    console.log(req.body)
+    res.send("lol")
+})
+
+
+mongoosse
+.connect('mongodb+srv://Ex0TiiC:Sunsun240205@nodelearn.9kus5.mongodb.net/nodeapi?retryWrites=true&w=majority&appName=Nodelearn')
+.then(()=>{
+    console.log("connected to mongoDB")
+
+    app.listen(port,()=>{
+        console.log("Succesfully connected to port",port)})
+
+
+}).catch((error)=>{
+    console.log(error)
+})
+
+
+
+// app.post('/regis',(req,res)=>{
+//     try {
+//         data = req.body
+//         const regisdata = {
+//             id : counter,
+//             user: data.user,
+//             password: data.password
+//         }
+//         if (regisdata.password.length <8){
+//             res.status(404).json({error:'Password must contain more than 8 characters'})
+//             console.log("Registeration failed")
+//             return
+//         }
+
+//         database.push(regisdata)
+//         res.status(200).json({status:"Registeration completed"})
+//         console.log("Registeration Completed")
+
+//     }
+
+//     catch{
+//         res.status(404).json({error:'An error occured'})
+//         console.log("Registeration failed")
+//     }
+//     console.log(database)
+// })
